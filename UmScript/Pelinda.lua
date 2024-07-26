7a910161ca4b1891fbf78e58ec285f97b90ed811
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2024-07-26 16:43:27 +0800

    Update Pelinda.lua

--- UmScript/Pelinda.lua
+++ UmScript/Pelinda.lua
@@ -1,4 +1,5 @@
-local ServiceID = "ashbornnhub"
+local S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11 = "a", "s", "h", "b", "o", "r", "n", "n", "h", "u", "b"
+local ServiceID = S1..S2..S3..S4..S5..S6..S7..S8..S9..S10..S11
 
 -- Load external libraries
 local function loadLibrary(url)
