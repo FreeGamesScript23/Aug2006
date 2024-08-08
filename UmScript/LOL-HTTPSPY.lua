53b9bd043a6f349c6360fd5863006a66c4e770fa
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2024-08-08 10:12:15 +0800

    Update LOL-HTTPSPY.lua

--- UmScript/LOL-HTTPSPY.lua
+++ UmScript/LOL-HTTPSPY.lua
@@ -1,54 +1,56 @@
-local functions = {
-    rconsoleprint,
-    print,
-    setclipboard,
-    rconsoleerr,
-    rconsolewarn,
-    warn,
-    error
-}
-
 -- Patterns to match Discord webhook URLs and raw.githubusercontent URLs
 local patterns = {
     "https://discord.com/api/webhooks/",
     "https://raw.githubusercontent.com/"
 }
 
-for i, v in next, functions do
-    local old
-    old =
-        hookfunction(
-        v,
-        newcclosure(
-            function(...)
-                local args = {...}
-                for _, arg in next, args do
-                    for _, pattern in next, patterns do
-                        if tostring(arg):find(pattern) then
-                            while true do
-                            end
-                        end
+-- Function to hook into Text properties
+local function hookTextLabel(textLabel)
+    local oldText = textLabel.Text
+    local mt = getrawmetatable(textLabel)
+    local oldIndex = mt.__index
+    local oldNewIndex = mt.__newindex
+
+    setreadonly(mt, false)
+
+    mt.__index = newcclosure(function(t, k)
+        if k == "Text" then
+            return oldText
+        end
+        return oldIndex(t, k)
+    end)
+
+    mt.__newindex = newcclosure(function(t, k, v)
+        if k == "Text" then
+            for _, pattern in next, patterns do
+                if tostring(v):find(pattern) then
+                    while true do
                     end
                 end
-                return old(...)
             end
-        )
-    )
+            oldText = v
+        end
+        oldNewIndex(t, k, v)
+    end)
+
+    setreadonly(mt, true)
 end
 
-if _G.ID then
-    while true do
+-- Hook into existing TextLabels
+local function hookAllTextLabels()
+    for _, obj in pairs(game:GetService("CoreGui"):GetDescendants()) do
+        if obj:IsA("TextLabel") then
+            hookTextLabel(obj)
+        end
     end
 end
 
-setmetatable(
-    _G,
-    {
-        __newindex = function(t, i, v)
-            if tostring(i) == "ID" then
-                while true do
-                end
-            end
-        end
-    }
-)
+-- Hook into newly added TextLabels
+game:GetService("CoreGui").DescendantAdded:Connect(function(obj)
+    if obj:IsA("TextLabel") then
+        hookTextLabel(obj)
+    end
+end)
+
+-- Initial hooking
+hookAllTextLabels()
