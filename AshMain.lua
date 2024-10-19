2ee800be48fde716b78ad76a1cf7e9e61dde7f29
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2024-10-19 14:26:50 +0800

    Update AshMain.lua

--- AshMain.lua
+++ AshMain.lua
@@ -44,6 +44,7 @@ local games = {
     [142823291] = 'Silent',
     [335132309] = 'Silent',
     [636649648] = 'Silent',
+    [73210641948512] = 'Silent',
     [70005410] = 'BloxHunt',
     [893973440] = 'FleeFacility',
 }
