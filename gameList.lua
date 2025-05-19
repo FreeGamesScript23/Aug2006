ac853ce3d7484ce260a38e23f1379ab0471ed705
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-05-19 09:50:35 +0800

    Create gameList.lua

---
+++ gameList.lua
@@ -0,0 +1,13 @@
+local games = {
+  [142823291] = "Silent",
+  [335132309] = "Silent",
+  [636649648] = "Silent",
+  [70005410] = "BloxHunt",
+  [893973440] = "FleeFacility",
+  [16732694052] = "aintnowaybro",
+  [72907489978215] = "aintnowaybro",
+  [2753915549] = 'bruhwhatisthis',
+  [7449423635] = 'bruhwhatisthis',
+}
+
+return games
