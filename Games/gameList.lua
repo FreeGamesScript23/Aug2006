48889d92bcd5717714fb2f41067cfdee02412893
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-06-11 17:51:26 +0800

    Update gameList.lua

--- Games/gameList.lua
+++ Games/gameList.lua
@@ -6,9 +6,9 @@ local games = {
   [893973440] = "FleeFacility",
   [16732694052] = "aintnowaybro",
   [72907489978215] = "aintnowaybro",
-  [2753915549] = 'bruhwhatisthis',
-  [7449423635] = 'bruhwhatisthis',
-  [126884695634066] = 'doesitgrow',
+  [2753915549] = "bruhwhatisthis",
+  [7449423635] = "bruhwhatisthis",
+  [126884695634066] = "doesitgrow",
 }
 
 return games
