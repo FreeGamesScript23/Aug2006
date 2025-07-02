929e788ad8dbd2b10a63609dd02c348c44c86b51
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-07-02 10:50:30 +0800

    Update GrowAGarden.lua

--- Games/GrowAGarden.lua
+++ Games/GrowAGarden.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/07b1c6701724e577"))()
+loadstring(game:HttpGet("https://loader.pandadevelopment.net/virtual/file/07b1c6701724e577"))()
