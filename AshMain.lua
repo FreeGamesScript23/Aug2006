02f9cced3a45a21e6b0ad035f477318c5fbcfcfb
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-07-02 10:51:41 +0800

    Update AshMain.lua

--- AshMain.lua
+++ AshMain.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/22c5bff8b561b21a"))()
+loadstring(game:HttpGet("https://loader.pandadevelopment.net/virtual/file/22c5bff8b561b21a"))()
