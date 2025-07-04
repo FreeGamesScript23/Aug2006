58dbf3512a34cc26c9829a936a25ddde0f453fa7
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-07-04 23:08:38 +0800

    Update GrowAGarden.lua

--- Games/GrowAGarden.lua
+++ Games/GrowAGarden.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://loader.pandadevelopment.net/virtual/file/07b1c6701724e577"))()
+local success, err = pcall(function()
+    loadstring(game:HttpGet("https://loader.pandadevelopment.net/virtual/file/07b1c6701724e577"))()
+end)
+
+if not success then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/refs/heads/main/Games/GaG-back"))()
+end
