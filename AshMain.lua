9a9bbd85f8576259d7584f71eb5cfac6745bffd5
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-07-04 23:14:02 +0800

    Update AshMain.lua

--- AshMain.lua
+++ AshMain.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://loader.pandadevelopment.net/virtual/file/22c5bff8b561b21a"))()
+local success, err = pcall(function()
+    loadstring(game:HttpGet("https://loader.pandadevelopment.net/virtual/file/22c5bff8b561b21a"))()
+end)
+
+if not success then
+    warn("Primary source failed, loading fallback...")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/refs/heads/main/AshMain-Modified.lua"))()
+end
