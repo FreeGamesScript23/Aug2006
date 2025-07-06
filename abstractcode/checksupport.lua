913fd483d2ac7ba53cb55e1e47b2600f8f9a8544
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-07-06 11:04:33 +0800

    Create checksupport.lua

---
+++ abstractcode/checksupport.lua
@@ -0,0 +1,57 @@
+local dataOwner = loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/main/Games/niggIds.lua", true))()
+local ownerUserIds = dataOwner.ownerUserIds
+local priorityRanks = dataOwner.priorityRanks
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait()
+
+local function CheckSupport()
+    local required = {
+        "hookfunction",
+        "hookmetamethod",
+        "request",
+        "fireproximityprompt",
+        "getconnections",
+        "getgc",
+        "getgenv",
+        "setreadonly",
+        "islclosure",
+        "newcclosure"
+    }
+    for _, v in ipairs(required) do
+        if typeof(getfenv()[v]) ~= "function" then
+            return false
+        end
+    end
+    return true
+end
+
+if ownerUserIds[LocalPlayer.UserId] then
+    print("LocalPlayer is an owner, bypassing checks.")
+else
+    if isfunctionhooked and restorefunction and CheckSupport() then
+        local functions = {
+            game.HttpPost,
+            game.HttpGet,
+            request,
+            getrawmetatable(game).__namecall,
+            Instance.new("RemoteEvent").FireServer,
+            Instance.new("RemoteFunction").InvokeServer
+        }
+        if syn and syn.request then
+            table.insert(functions, syn.request)
+        end
+        task.spawn(function()
+            while true do
+                for _, func in ipairs(functions) do
+                    if func and isfunctionhooked(func) then
+                        restorefunction(func)
+                    end
+                end
+                task.wait(0.5)
+            end
+        end)
+    else
+        LocalPlayer:Kick("Required function missing. Change your executor - dsc.gg/AshbornnHub")
+    end
+end
