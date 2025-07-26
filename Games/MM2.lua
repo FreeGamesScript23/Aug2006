5c1b5a0709af7c53460764f5304d22bb93c92ba6
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-07-26 23:17:31 +0800

    Update MM2.lua

--- Games/MM2.lua
+++ Games/MM2.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/1750da7174ed6675"))()
+local success, err = pcall(function()
+    loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/1750da7174ed6675"))()
+end)
+
+if not success then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/refs/heads/main/Games/MM2-Backup"))()
+end
