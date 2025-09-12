c0c47a970d2b03d4545bfaaaa9299332f8b51b97
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-09-12 22:39:01 +0800

    Update AshMain.lua

--- AshMain.lua
+++ AshMain.lua
@@ -1,8 +1,1 @@
-local success, err = pcall(function()
-    loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/c0d905745c0457b4"))()
-end)
-
-if not success then
-    warn("LLD Source Failed:", err)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/FreeGamesScript23/Aug2006/refs/heads/main/AshMain-Modified.lua"))()
-end
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/d236ecda76097022"))()
