a4a220093ab9e1d5a652f87ccad0bb8324912ad7
FreeGamesScript23 <129659928+FreeGamesScript23@users.noreply.github.com>
2025-10-24 18:30:51 +0800

    Update FlingGood.lua

--- FlingGood.lua
+++ FlingGood.lua
@@ -237,11 +237,6 @@ function flingloopfix()
         end
     end
 
-    if not Welcome then
-        Message("Hub Says:", "Fling Script has been loaded", 3)
-    end
-    getgenv().Welcome = true
-
     if Targets[1] then
         for _, x in next, Targets do
             GetPlayer(x)
